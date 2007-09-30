bb23963bbbfc52b0d76d4af466164d67b45429b4
damian.eads <damian.eads@01130180-ee3a-0410-ab2d-0fd08979ac96>
2007-09-30 19:15:07 +0000

    Fixed error in squareform documentation.

--- trunk/src/cluster.py
+++ trunk/src/cluster.py
@@ -331,14 +331,16 @@ def squareform(X, force="no", checks=True):
       Given a square dxd symmetric distance matrix X, v=squareform(X)
       returns a d*(d-1)/2 (n \choose 2) sized vector v.
 
-      v[(i + 1) \choose 2 + j] is the distance between points i and j.
-      If X is non-square or asymmetric, an error is returned.
+      v[(n \choose 2)-(n-i \choose 2) + (j-i-1)] is the distance
+      between points i and j. If X is non-square or asymmetric, an error
+      is returned.
 
     X = squareform(v)
 
       Given a d*d(-1)/2 sized v for some integer d>=2 encoding distances
       as described, X=squareform(v) returns a dxd distance matrix X. The
-      X[i, j] and X[j, i] value equals v[(i + 1) \choose 2 + j] and all
+      X[i, j] and X[j, i] values are set to
+      v[(n \choose 2)-(n-i \choose 2) + (j-u-1)] and all
       diagonal elements are zero.
 
     As with MATLAB(R), if force is equal to 'tovector' or 'tomatrix',
