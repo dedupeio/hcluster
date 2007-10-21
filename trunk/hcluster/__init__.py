1c7dc3465ddd1ba23d68fb5576f68dbc420673e6
damian.eads <damian.eads@01130180-ee3a-0410-ab2d-0fd08979ac96>
2007-10-21 23:01:40 +0000

    Moved source files to a different directory.

---
+++ trunk/hcluster/__init__.py
@@ -0,0 +1,13 @@
+import cluster as _cp
+from cluster import __doc__ as _ds
+from cluster import *
+
+__doc__ = _ds
+
+def all():
+    """
+    all() gives help for all functions in hcluster.
+    """
+    help(_cp)
+
+
