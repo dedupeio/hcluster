618cdf059d9edeaf664abe0be517649f59737417
damian.eads <damian.eads@01130180-ee3a-0410-ab2d-0fd08979ac96>
2007-11-13 03:39:15 +0000

    Added references to the documentation.

--- trunk/hcluster/cluster.py
+++ trunk/hcluster/cluster.py
@@ -25,7 +25,7 @@ Agglomorative cluster formation
 
 Distance matrix computation from a collection of raw observation vectors
 
- pdist              computes the distance between each point given a metric.
+ pdist              computes distances between each observation pair.
  squareform         converts a sq. D.M. to a condensed one and vice versa.
 
 Statistic computations on hierarchies
@@ -92,9 +92,47 @@ Legal stuff
 
  copying            Displays the license for this package.
 
-MATLAB and MathWorks are registered trademarks of The MathWorks, Inc.
-Mathematica is a registered trademark of The Wolfram Research, Inc.
 
+  MATLAB and MathWorks are registered trademarks of The MathWorks, Inc.
+  Mathematica is a registered trademark of The Wolfram Research, Inc.
+
+References:
+
+ [1] "Statistics toolbox." API Reference Documentation. The MathWorks.
+     http://www.mathworks.com/access/helpdesk/help/toolbox/stats/.
+     Accessed October 1, 2007.
+
+ [2] "Hierarchical clustering." API Reference Documentation.
+     The Wolfram Research, Inc. http://reference.wolfram.com/...
+     ...mathematica/HierarchicalClustering/tutorial/...
+     HierarchicalClustering.html. Accessed October 1, 2007.
+     
+ [3] Gower, JC and Ross, GJS. "Minimum Spanning Trees and Single Linkage
+     Cluster Analysis." Applied Statistics. 18(1): pp. 54--64. 1969.
+
+ [4] Ward Jr, JH. "Hierarchical grouping to optimize an objective
+     function." Journal of the American Statistical Association. 58(301):
+     pp. 236--44. 1963.
+
+ [5] Johnson, SC. "Hierarchical clustering schemes." Psychometrika.
+     32(2): pp. 241--54. 1966.
+
+ [6] Sneath, PH and Sokal, RR. "Numerical taxonomy." Nature. 193: pp.
+     855--60. 1962.
+
+ [7] Batagelj, V. "Comparing resemblance measures." Journal of
+     Classification. 12: pp. 73--90. 1995.
+
+ [8] Sokal, RR and Michener, CD. "A statistical method for evaluating
+     systematic relationships." Scientific Bulletins. 38(22):
+     pp. 1409--38. 1958.
+
+ [9] Edelbrock, C. "Mixture model tests of hierarchical clustering
+     algorithms: the problem of classifying everybody." Multivariate
+     Behavioral Research. 14: pp. 367--84. 1979.
+
+[10] Jain, A., and Dubes, R., "Algorithms for Clustering Data."
+     Prentice-Hall. Englewood Cliffs, NJ. 1988.
 """
 
 _copyingtxt="""
