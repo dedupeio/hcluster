fab53a59da7f689d14e7e208a72d9142d170f289
damian.eads <damian.eads@01130180-ee3a-0410-ab2d-0fd08979ac96>
2008-06-10 16:39:25 +0000

    Tweaked setup.py to work with pypi.

--- trunk/setup.py
+++ trunk/setup.py
@@ -49,16 +49,37 @@ extra_link_args = []
 setup(name='hcluster', \
       version='0.1.8', \
       py_modules=['hcluster.cluster'], \
-      description='A hierarchical clustering package written for Scipy.', \
-      long_description='A hierarchical clustering package written in C and Python.', \
-      ext_modules=[Extension('_cluster_wrap', \
-                             ['hcluster/cluster.c', 'hcluster/cluster_wrap.c'], \
-                             extra_link_args = extra_link_args, \
-                             include_dirs=['hcluster/', include_numpy_array])], \
-      keywords=['dendrogram', 'linkage', 'cluster', 'agglomorative', 'hierarchical', 'hierarchy'], \
-      author="Damian Eads", \
-      author_email="damian XDOTX eads XATX gmail XDOTX com", \
-      license="New BSD License", \
-      packages = ['hcluster'], \
-      url = 'http://scipy-cluster.googlecode.com', \
+      description='A hierarchical clustering package for Scipy.', \
+      long_description="""
+This library provides Python functions for hierarchical clustering. Its features
+include
+
+    * generating hierarchical clusters from distance matrices
+    * computing distance matrices from observation vectors
+    * computing statistics on clusters
+    * cutting linkages to generate flat clusters
+    * and visualizing clusters with dendrograms. 
+
+The interface is very similar to MATLAB's Statistics Toolbox API to make code
+easier to port from MATLAB to Python/Numpy. The core implementation of this
+library is in C for efficiency.
+""",
+      ext_modules=[Extension('_cluster_wrap',
+                             ['hcluster/cluster.c', 'hcluster/cluster_wrap.c'],
+                             extra_link_args = extra_link_args,
+                             include_dirs=['hcluster/', include_numpy_array])], 
+      keywords=['dendrogram', 'linkage', 'cluster', 'agglomorative', 'hierarchical', 'hierarchy', 'ward', 'distance'],
+      author="Damian Eads",
+      author_email="damian XDOTX eads XATX gmail XDOTX com",
+      license="New BSD License",
+      packages = ['hcluster'],
+      classifiers = ["Topic :: Scientific/Engineering :: Information Analysis",
+                     "Topic :: Scientific/Engineering :: Artificial Intelligence",
+                     "Topic :: Scientific/Engineering :: Bio-Informatics",
+                     "Programming Language :: Python",
+                     "Operating System :: OS Independent",
+                     "License :: OSI Approved :: BSD License",
+                     "Intended Audience :: Science/Research",
+                     "Development Status :: 4 - Beta"],
+      url = 'http://scipy-cluster.googlecode.com',
       )
