c63be4a17ffea24b416493ac49448902c5c60efc
Forest Gregg <fgregg@uchicago.edu>
2018-08-07 15:55:07 -0500

    bump version due to changes in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ else:
                              include_dirs=[numpy.get_include()])]
 
 setup(maintainer="Forest Gregg",
-      version="0.3.2",
+      version="0.3.3",
       name='dedupe-hcluster',
       packages=['hcluster'],
       maintainer_email="fgregg@datamade.us",
